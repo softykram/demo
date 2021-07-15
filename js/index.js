0095c89f1417fd4f6b9f5b73c7cd192c1649f9b7
softykram <cartaloco@gmail.com>
2021-07-15 00:16:19 -0500

    index
    creación demo

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      navigation : true,
+      navigationPosition : 'right',
+      navigationTooltips: ['INICIO', 'SERVICIOS', 'PORTAFOLIO','PROCESO', 'CONTACTENOS'],
+      slidesNavigation : true,
+      slidesNavPosition : 'bottom',
+      controlArrows: false,
+    });
+});
+
